826ad28a1e34a6800ab8d92d7fd903ad9dc8d33b
Aleš Omerzel <omerzel.vales@gmail.com>
2016-09-29 13:56:30 +0200

    Create 2 view pages

---
+++ app/src/androidTest/java/ch/ethz/coss/android_view_page_example/ApplicationTest.java
@@ -0,0 +1,13 @@
+package ch.ethz.coss.android_view_page_example;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
